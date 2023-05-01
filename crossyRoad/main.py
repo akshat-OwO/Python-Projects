7ea7caee2ca2ce46fd6b74555e3c3aa9daf4b022
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-05-01 23:00:59 +0530

    turtle crossing using classes

---
+++ crossyRoad/main.py
@@ -0,0 +1,35 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkeypress(player.move, 'Up')
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    if player.ycor() > 280:
+        player.level_completed()
+        scoreboard.increase_level()
+        car_manager.level_up()
+    
+    if car_manager.collision(player):
+        scoreboard.game_over()
+        game_is_on = False
+
+screen.exitonclick()
